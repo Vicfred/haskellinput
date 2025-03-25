c93c4ba69385750997fd8313b3a5b8d664dfe339
vicfred <git@vicfred.dev>
2025-03-25 01:51:56 -0600

    n then n lines

---
+++ abc364a_glutton_takahashi.hs
@@ -0,0 +1,10 @@
+-- https://atcoder.jp/contests/abc364/tasks/abc364_a
+-- single integer then read n lines
+import Control.Monad
+
+main :: IO ()
+main = do
+  n <- read <$> getLine :: IO Int
+  s <- replicateM n getLine
+  print n
+  print s

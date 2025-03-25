d673a2614adbbe64642aa5aeb38a8c916e822089
vicfred <git@vicfred.dev>
2025-03-25 17:37:14 -0600

    list of pairs

--- 003_list_of_pairs.hs
+++ 003_list_of_pairs.hs
@@ -1,6 +1,8 @@
 -- https://atcoder.jp/contests/abc383/tasks/abc383_a
 -- list of pairs
-import Control.Monad
+import Control.Applicative
+import Control.Monad (replicateM)
+import Data.List (transpose)
 
 main :: IO ()
 main = do
@@ -13,7 +15,12 @@ main = do
   print ts
   print vs
   -}
+  {-
   pairs <- replicateM n $ do
     [t, v] <- map read . words <$> getLine :: IO [Int]
     return (t, v)
   mapM_ print pairs
+  -}
+  [ts, vs] <- transpose <$> replicateM n (map read . words <$> getLine) :: IO [[Int]]
+  print ts
+  print vs
